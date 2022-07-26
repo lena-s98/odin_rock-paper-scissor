1a4d251554cd0bca8a51c059a890167c82cca266
Lena <lena.sterz98@gmail.com>
2022-07-26 17:19:23 +0200

    Fix the game and add comments besides the mistake the author made

--- script.js
+++ script.js
@@ -1,72 +1,70 @@
-let playerSelection = prompt("Type rock, paper or scissor").toLowerCase();
+let playerSelection = "";
 let computerSelection = computerChoice();
-let result = playOneRound(playerSelection, computerSelection);
 let playerScore = 0;
 let computerScore = 0;
+const win = "You win this round";
+const lose = "You lose this round";
+const tie = "It's a tie";
 
 
 // This function generates a random number between 0 and 2.
 // Then it declares rock, paper or scissor depending on the number.
-function computerChoice() {
+ function computerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
-    switch(randomNumber) {
-        case 0:
-            return "rock";
-        case 1:
-            return "paper";
-        case 2:
-            return "scissor";
-    }
-}
+     switch(randomNumber) {
+         case 0:
+             return "rock";
+         case 1:
+             return "paper";
+         case 2:
+             return "scissor";
+     }
+ }
 
-console.log(computerSelection);
 
 // This function will play one round of rock, paper, scissor in the console.
 // It will take the players choice and the random computers choice then compare them with eachother.
 // After the comparison it will declare a winner depending on the comparison.
 function playOneRound(playerSelection, computerSelection) {
     if(playerSelection === computerSelection) {
-        return "It's a tie";
-    } else if((playerSelection === "rock" && computerSelection === "scissor")) {
-        return "You win!";
-    } else if((playerSelection === "scissor" && computerSelection === "paper")) {
-        return "You win!";
-    } else if((playerSelection === "paper" && computerSelection === "rock")) {
-        return "You win!";
-    } else if((computerSelection === "rock" && playerSelection === "scissor")) {
-        return "The computer wins";
-    } else if((computerSelection === "scissor" && playerSelection === "paper")) {
-        return "The computer wins";
-    } else if((computerSelection === "paper" && playerSelection === "rock")) {
-        return "The computer wins";
+        return tie;
+    } else if((playerSelection === "rock" && computerSelection === "scissor") || (playerSelection === "scissor" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "rock")) {
+        playerScore++; //---Ich musste das Doppelplus hinter die Variable schreiben---
+        return win;
+    } else if((computerSelection === "rock" && playerSelection === "scissor") || (computerSelection === "scissor" && playerSelection === "paper") || (computerSelection === "paper" && playerSelection === "rock")) {
+        computerScore++;
+        return lose;
+    } else {
+        console.log("Please put in rock, paper or scissor");
     }
 }
 
-// This function increases the score.
-function increaseScore(playerScore, computerScore) {
-    if(result === "It's a tie") {
-        console.log(playerScore, computerScore);
-    } else if(result === "You win!") {
-        ++playerScore;
-        console.log(playerScore, computerScore);
-    } else if(result === "The computer wins") {
-        ++computerScore;
-        console.log(playerScore, computerScore);
-    }
-}
-
-console.log(playOneRound(playerSelection, computerSelection));
-console.log(increaseScore(playerScore, computerScore));
 
 // This function asks for a new prompt anytime it's called.
 function resetPlayerSelection() {
     playerSelection = prompt("Type rock, paper or scissor").toLowerCase();
 }
+//---Die Reset-Funktion hätte ich als solches nicht gebraucht. Es reicht eine leere Variable, die
+//in der Loop geupdatet wird---
+
 
 // This function will loop the game 5 times.
 function game() {
-    for(let i = 1; i <= 5; i++) {
-        playOneRound(playerSelection, computerSelection);
-        resetPlayerSelection();
+    for(let i = 0; i < 5; i++) {
+        playerSelection = prompt("Choose Rock, Paper or Scissor: ").toLowerCase();
+        console.log("You choose " + playerSelection);
+        computerChoice(); //---Diese Funktion aufzurufen, um eine neue Computerauswahl zu erhalten hat ausgereicht---
+        console.log("The computer chooses " + computerSelection);
+        console.log(playOneRound(playerSelection, computerSelection));
     }
+    if(playerScore > computerScore) {
+        return "You win this game :)";
+    } else if(playerScore < computerScore) {
+        return "You lose this game";
+    } else {
+        return "It's a tie";
+    } //---Die if Funktion, die die Punkte vergleicht musste außerhalb der Loop stehen. Dann hat alles funktioniert ^-^ ---
 }
+
+console.log(game());
+console.log(`Your score is ${playerScore} and the computer's score is ${computerScore}`);
