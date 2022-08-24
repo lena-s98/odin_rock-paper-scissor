892789876439e1c54e27f3cae82140641eb7b8dc
Lena <lena.sterz98@gmail.com>
2022-08-24 17:01:16 +0200

    add DOM manipulation to update choice, score and round winner

--- script.js
+++ script.js
@@ -1,78 +1,84 @@
-var computerSelection = '';
-var playerSelection = '';
+var computerSelection = "";
+var playerSelection = "";
 var playerScore = 0;
 var computerScore = 0;
-const win = "You win this round";
-const lose = "You lose this round";
-const tie = "It's a tie";
 
-const resultsContainer = document.querySelector('.results');
+const resultsContainer = document.querySelector(".results");
 
-const playerScoreTitle = document.createElement('h2');
+const roundResult = document.createElement("h3");
+resultsContainer.appendChild(roundResult);
+
+var choiceDisplay = document.createElement("h2");
+resultsContainer.appendChild(choiceDisplay);
+
+const playerScoreTitle = document.createElement("h2");
 playerScoreTitle.textContent = ` Your score is: ${playerScore}`;
 resultsContainer.appendChild(playerScoreTitle);
 
-const computerScoreTitle = document.createElement('h2');
+const computerScoreTitle = document.createElement("h2");
 computerScoreTitle.textContent = ` The computers score is: ${computerScore}`;
 resultsContainer.appendChild(computerScoreTitle);
+
+const gameWinner = document.createElement("p");
+resultsContainer.appendChild(gameWinner);
 
 // This function generates a random number between 0 and 2.
 // Then it declares rock, paper or scissor depending on the number.
 function computerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3);
-  switch (randomNumber) {
-    case 0:
-      return "rock";
-    case 1:
-      return "paper";
-    case 2:
-      return "scissor";
-  }
+	let randomNumber = Math.floor(Math.random() * 3);
+	switch (randomNumber) {
+		case 0:
+			return "rock";
+		case 1:
+			return "paper";
+		case 2:
+			return "scissor";
+	}
 }
-
-// Eventlistener for the playerselection buttons
-const playerButtons = document.querySelectorAll('button');
-playerButtons.forEach((button) => button.addEventListener('click', () => {
-  playerSelection = button.value;
-  console.log(playOneRound(playerSelection, computerSelection));
-}));
 
 // This function will play one round of rock, paper, scissor in the console.
 // It will take the players choice and the random computers choice then compare them with eachother.
 // After the comparison it will declare a winner depending on the comparison.
 function playOneRound(playerSelection, computerSelection) {
-  //Computerchoice gets called and stored in the computerSelection variable.
-  computerChoice();
-  computerSelection = computerChoice();
-  console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}`)
+	//Computerchoice gets called and stored in the computerSelection variable.
+	computerChoice();
+	computerSelection = computerChoice();
+	choiceDisplay.textContent = `You chose ${playerSelection} and the computer chose ${computerSelection}`;
 
-  if (playerSelection === computerSelection) {
-    return tie;
-  } else if (
-    (playerSelection === "rock" && computerSelection === "scissor") ||
-    (playerSelection === "scissor" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "rock")
-  ) {
-    playerScore++; //---Ich musste das Doppelplus hinter die Variable schreiben---
-    return win;
-  } else if (
-    (computerSelection === "rock" && playerSelection === "scissor") ||
-    (computerSelection === "scissor" && playerSelection === "paper") ||
-    (computerSelection === "paper" && playerSelection === "rock")
-  ) {
-    computerScore++;
-    return lose;
-  } else {
-    console.log("Please put in rock, paper or scissor");
-  }
+	if (playerSelection === computerSelection) {
+		roundResult.textContent = "It's a tie";
+	} else if (
+		(playerSelection === "rock" && computerSelection === "scissor") ||
+		(playerSelection === "scissor" && computerSelection === "paper") ||
+		(playerSelection === "paper" && computerSelection === "rock")
+	) {
+		playerScore++;
+		playerScoreTitle.textContent = ` Your score is: ${playerScore}`;
+		roundResult.textContent = "You win this round";
+	} else if (
+		(computerSelection === "rock" && playerSelection === "scissor") ||
+		(computerSelection === "scissor" && playerSelection === "paper") ||
+		(computerSelection === "paper" && playerSelection === "rock")
+	) {
+		computerScore++;
+		computerScoreTitle.textContent = ` The computers score is: ${computerScore}`;
+		roundResult.textContent = "You lose this round";
+	}
 }
+
+// Eventlistener for the playerselection buttons
+const playerButtons = document.querySelectorAll("button");
+playerButtons.forEach((button) =>
+	button.addEventListener("click", () => {
+		playerSelection = button.value;
+		playOneRound(playerSelection, computerSelection);
+	})
+);
 
 // This function asks for a new prompt anytime it's called.
 // function resetPlayerSelection() {
 //   playerSelection = prompt("Type rock, paper or scissor").toLowerCase();
 // }
-
-
 
 //---Die Reset-Funktion hätte ich als solches nicht gebraucht. Es reicht eine leere Variable, die
 //in der Loop geupdatet wird---
@@ -94,7 +100,3 @@ function playOneRound(playerSelection, computerSelection) {
 //     return "It's a tie";
 //   } //---Die if Funktion, die die Punkte vergleicht musste außerhalb der Loop stehen. Dann hat alles funktioniert ^-^ ---
 //}
-
-console.log(
-  `Your score is ${playerScore} and the computer's score is ${computerScore}`
-);
